e4dc13668336bd356a784ce3df9af5494bd51eb9
byodt <ceo.byodt@gmail.com>
2024-03-13 17:37:34 -0300

    busqueda  por  ubicion actual lista

--- src/app/pages/buscar/buscar.page.ts
+++ src/app/pages/buscar/buscar.page.ts
@@ -1,41 +1,49 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { NavController } from '@ionic/angular';
-import { ToastController } from '@ionic/angular';
-import { FirestoreService } from 'src/app/services/firestore.service';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { UtilsService } from 'src/app/services/utils.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AlertController } from '@ionic/angular';
-import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import 'firebase/firestore';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { take } from 'rxjs/operators';
-import { ItemReorderEventDetail } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { combineLatest } from 'rxjs';
 
 
-interface Producto {
-  uid: any;
+
+
+
+interface ServicioData {
+  coordenadasUsuario: { lat: number; lng: number; };
+  nombreUsuario: string;
+  uidUsuario: string;
   nombre: string;
   descripcion: string;
-  idUsuario: string; // Agrega esta propiedad si tus productos tienen un ID de usuario asociado
-  latitud: number; // Agrega esta propiedad si tus productos tienen una latitud
-  longitud: number; // Agrega esta propiedad si tus productos tienen una longitud
+  uid: string;
 }
 
-interface Servicio {
-  uid: any;
+// Define una interfaz para la estructura de tus documentos de producto
+interface ProductoData {
+  coordenadasUsuario: { lat: number; lng: number; };
+  nombreUsuario: string;
+  uidUsuario: string;
   nombre: string;
   descripcion: string;
-  idUsuario: string; // Agrega esta propiedad si tus servicios tienen un ID de usuario asociado
-  latitud: number; // Agrega esta propiedad si tus servicios tienen una latitud
-  longitud: number; // Agrega esta propiedad si tus servicios tienen una longitud
+  uid: string;
+}
+
+
+
+interface UserData {
+  name: string;
+  coordenadas: { lat: number, lng: number }; // Suponiendo que las coordenadas son un objeto con las propiedades lat y lng
+  // Otros campos si los tienes
+}
+export interface ProductoConUsuario extends ProductoData {
+  nombreUsuario: string;
+  coordenadasUsuario: { lat: number; lng: number };
+  
+}
+export interface ServicioConUsuario extends ProductoData {
+  nombreUsuario: string;
+  coordenadasUsuario: { lat: number; lng: number };
+  
 }
 
 
@@ -49,32 +57,28 @@ export class BuscarPage implements OnInit {
   paginaActual: string;
   terminoDeBusqueda: string = ''; // Declaración de la propiedad terminoDeBusqueda
   ubicacionSeleccionada: string;
-  servicios: Servicio[] = [];
+  servicios: any [];
   productos: any[];
   productosEncontrados: { nombre: string, descripcion: string }[] = [];  
   serviciosEncontrados:  { nombre: string, descripcion: string }[] = [];
-tipoBusqueda: string = 'producto'; 
+  tipoBusqueda: string = 'producto'; 
   
+
+
 
   constructor(
     private router: Router,
-    private navCtrl: NavController,
-    private toastController: ToastController,
-    private firestoreService: FirestoreService,
-    private firebaseService: FirebaseService,
-    private utilsService: UtilsService,
-    private alertController: AlertController,
-    private formBuilder: FormBuilder,
-    private afAuth: AngularFireAuth, 
     private firestore: AngularFirestore,
     ) {
-      
+      this.productosEncontrados = [];
+      this.serviciosEncontrados = [];
   }
  
 
   ngOnInit() {
     this.buscarProductos();
     this.buscarServicios();
+    this.ubicacionSeleccionada = 'actual';
   }
 
   navigateTo(route: string) {
@@ -86,65 +90,101 @@ cambiarPagina(event) {
 }
 
 buscarServicios() {
-  this.firestore.collectionGroup('servicios', ref => ref.where('nombre', '==', this.terminoDeBusqueda))
-    .valueChanges().subscribe((servicios: Servicio[]) => {
-      console.log('Servicios encontrados:', servicios);
-      this.serviciosEncontrados = servicios;
-      
-      servicios.forEach(servicio => {
-        // Obtener el UID del usuario del servicio
-        const uidUsuario = servicio.uid; // Ajusta según la estructura real de tu base de datos
-        console.log('UID del usuario:', uidUsuario);
-        
-        // Buscar el usuario usando el UID obtenido
-        this.firestore.collection('usuarios').doc(uidUsuario).valueChanges().subscribe((usuario: any) => {
-          console.log('Usuario encontrado:', usuario);
-          if (usuario && usuario.name && usuario.coordenadas && usuario.coordenadas.lat && usuario.coordenadas.lng) {
-            console.log('Nombre del usuario:', usuario.name);
-            console.log('Coordenadas del usuario:', usuario.coordenadas.lat, usuario.coordenadas.lng);
-          } else {
-            console.log('El usuario o sus propiedades son indefinidas');
-          }
+  if (this.terminoDeBusqueda.trim() !== '') {
+    this.firestore.collectionGroup('servicios', ref => ref.where('nombre', '==', this.terminoDeBusqueda))
+      .get().subscribe(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const servicioData = doc.data() as ServicioData;
+          const uidUsuario = doc.ref.parent.parent?.id;
+          console.log('el UID es', uidUsuario );
+          servicioData.uidUsuario = uidUsuario;
+
+          // Obtener el documento del usuario usando el UID
+          this.firestore.collection('usuarios').doc(uidUsuario).get()
+            .subscribe(usuarioDoc => {
+              if (usuarioDoc.exists) {
+                const usuarioData = usuarioDoc.data() as UserData;
+                const coordenadasUsuario = usuarioData.coordenadas;
+
+                // Guardar los datos relevantes en variables
+                const nombreServicio = servicioData.nombre;
+                const descripcionServicio = servicioData.descripcion;
+                const nombreUsuario = usuarioData.name;
+
+                // Navegar a la página de ubicación con los parámetros de consulta
+                this.router.navigate(['/ubication'], {
+                  queryParams: {
+                    nombreProducto: nombreServicio,
+                    descripcionProducto: descripcionServicio,
+                    nombreUsuario: nombreUsuario,
+                    coordenadasUsuario: JSON.stringify(coordenadasUsuario)
+                  }
+                });
+              } else {
+                console.error('No se encontró el usuario con el UID:', uidUsuario);
+              }
+            }, error => {
+              console.error('Error al obtener el usuario:', error);
+            });
         });
+      }, (error) => {
+        console.error('Error al buscar servicios:', error);
+        this.mostrarMensajeError();
       });
-    }, (error) => {
-      console.error('Error al buscar servicios:', error);
-      this.mostrarMensajeError();
-    });
+  } else {
+    console.log('El término de búsqueda está vacío');
+  }
 }
+
+
+
 
 buscarProductos() {
-  this.firestore.collectionGroup('productos', ref => ref.where('nombre', '==', this.terminoDeBusqueda))
-    .valueChanges().subscribe((productos: Producto[]) => {
-      console.log('Productos encontrados:', productos);
-      this.productosEncontrados = productos;
-      
-      productos.forEach(producto => {
-        // Obtener el UID del usuario del producto
-        const uidUsuario = producto.uid; // Ajusta según la estructura real de tu base de datos
-        console.log('UID del usuario:', uidUsuario);
-        
-        // Buscar el usuario usando el UID obtenido
-        this.firestore.collection('usuarios').doc(uidUsuario).valueChanges().subscribe((usuario: any) => {
-          console.log('Usuario encontrado:', usuario);
-          if (usuario && usuario.name && usuario.coordenadas && usuario.coordenadas.lat && usuario.coordenadas.lng) {
-            console.log('Nombre del usuario:', usuario.name);
-            console.log('Coordenadas del usuario:', usuario.coordenadas.lat, usuario.coordenadas.lng);
-          } else {
-            console.log('El usuario o sus propiedades son indefinidas');
-          }
+  if (this.terminoDeBusqueda.trim() !== '') {
+    this.firestore.collectionGroup('productos', ref => ref.where('nombre', '==', this.terminoDeBusqueda))
+      .get().subscribe(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const productoData = doc.data() as ProductoData;
+          const uidUsuario = doc.ref.parent.parent?.id;
+          console.log('el UID es', uidUsuario );
+          productoData.uidUsuario = uidUsuario;
+
+          // Obtener el documento del usuario usando el UID
+          this.firestore.collection('usuarios').doc(uidUsuario).get()
+            .subscribe(usuarioDoc => {
+              if (usuarioDoc.exists) {
+                const usuarioData = usuarioDoc.data() as UserData;
+                const coordenadasUsuario = usuarioData.coordenadas;
+
+                // Guardar los datos relevantes en variables
+                const nombreProducto = productoData.nombre;
+                const descripcionProducto = productoData.descripcion;
+                const nombreUsuario = usuarioData.name;
+
+                // Navegar a la página de ubicación con los parámetros de consulta
+                this.router.navigate(['/ubication'], {
+                  queryParams: {
+                    nombreProducto: nombreProducto,
+                    descripcionProducto: descripcionProducto,
+                    nombreUsuario: nombreUsuario,
+                    coordenadasUsuario: JSON.stringify(coordenadasUsuario)
+                  }
+                });
+              } else {
+                console.error('No se encontró el usuario con el UID:', uidUsuario);
+              }
+            }, error => {
+              console.error('Error al obtener el usuario:', error);
+            });
         });
+      }, (error) => {
+        console.error('Error al buscar productos:', error);
+        this.mostrarMensajeError();
       });
-    }, (error) => {
-      console.error('Error al buscar productos:', error);
-      this.mostrarMensajeError();
-    });
+  } else {
+    console.log('El término de búsqueda está vacío');
+  }
 }
-
-
-
-
-
 
 
 
@@ -163,6 +203,7 @@ buscar() {
     this.buscarServicios();
   }
 }
+
 
 
 
--- src/app/pages/ubication/ubication.page.ts
+++ src/app/pages/ubication/ubication.page.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { DataStorageService } from 'src/app/services/data-storage.service';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
 
-declare var google; 
+declare var google: any;
 
 @Component({
   selector: 'app-ubication',
@@ -13,116 +11,101 @@ declare var google;
   styleUrls: ['./ubication.page.scss'],
 })
 export class UbicationPage implements OnInit {
-  data: any;
 
-  map: any; // Declara la variable para el mapa
-  infoWindows: any = [];
-  marcador: any = [];
- 
-  datos: any = [];
-  constructor(private dataStorage: DataStorageService,
-              private firebaseServ: FirebaseService,
-              private  router: Router,
-              private http: HttpClient
-              ){ }
-
-
-  async ngOnInit() {
-    
-    
-    // Espera a que se agreguen los marcadores antes de continuar
- 
-
-    this.initMap();
-
-  }
-
-
-  signOut() {
-    // Eliminar el token de autenticación de localStorage
-    localStorage.removeItem('userToken');
-
-    this.dataStorage.clearDataGym();
-    
-    // Llamar al método de signOut de Firebase si es necesario
-    this.firebaseServ.signOut();
-    
-    // Navegar a la página de inicio
-    this.router.navigate(['/']);
-  }
-
-  openMain() {
-    // Abre la página de "Ubicaciones" con un pequeño retraso
-    setTimeout(() => {
-      this.router.navigate(['/main']);
-    }, 400); // 300 milisegundos (ajusta este valor según tus necesidades)
-  }
-  initMap() {
-    const mapEle: HTMLElement = document.getElementById('map');
-    
-    const myLatLng = { lat: -33.03356325015952, lng: -71.53317337493256 };
-    this.map = new google.maps.Map(mapEle, {
-      center: myLatLng,
-      zoom: 13
-    });
-    google.maps.event.addListenerOnce(this.map, 'idle', () => {
-      mapEle.classList.add('show-map');
-      this.addMarkersToMap(this.marcador);
-    });
-  }
-
+  map: any;
+  infoWindows: any[] = [];
+  markers: any[] = [];
   
- addMarkersToMap(marcador): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      for (let marker of marcador) {
-        let position = new google.maps.LatLng(parseFloat(marker.latitud), parseFloat(marker.longitud));
-        let mapMarker = new google.maps.Marker({
-          position: position,
-          nombre: marker.nombre,
-          latitud: marker.latitud,
-          longitud: marker.longitud,
-          direccion: marker.direccion,
-          horario: marker.horario
-        });
-        mapMarker.setMap(this.map);
-        this.addInfoWindowToMarker(mapMarker);
-      }
-      resolve(); // Resuelve la promesa una vez que todos los marcadores se han agregado.
+  constructor(
+    private router: Router,
+    private firestore: AngularFirestore,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      const nombreProducto = params['nombreProducto'];
+      const descripcionProducto = params['descripcionProducto'];
+      const nombreUsuario = params['nombreUsuario'];
+      const coordenadasUsuario = JSON.parse(params['coordenadasUsuario']);
+
+      // Llamar a la función para inicializar el mapa y agregar marcadores
+      this.initMap(coordenadasUsuario, nombreProducto, descripcionProducto, nombreUsuario);
     });
+
+    // Obtener la ubicación actual del usuario al cargar la página
+    this.obtenerUbicacionActual();
   }
-
-
-  addInfoWindowToMarker(marker) {
-    let infoWindowContent = '<div id="content" class="info-window">' +
-      '<h2 id="firstHeading" class="firstHeading">' + marker.nombre + '</h2>' +
-      '<p>Direccion: ' + marker.direccion + '</p>' +
-      '<p>Horario: ' + marker.horario + '</p>' +     
-      '</div>';
   
-    let infoWindow = new google.maps.InfoWindow({
-      content: infoWindowContent
+  initMap(coordenadasUsuario: any, nombreProducto: string, descripcionProducto: string, nombreUsuario: string) {
+    // Crear el mapa si aún no está creado
+    if (!this.map) {
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: coordenadasUsuario,
+        zoom: 12
+      });
+    }
+
+    // Crear un marcador en las coordenadas recibidas
+    const marker = new google.maps.Marker({
+      position: coordenadasUsuario,
+      map: this.map,
+      title: nombreProducto
     });
   
+    // Crear el contenido del infowindow del marcador
+    const contentString = `
+      <div>
+        <h3>${nombreUsuario}</h3>
+        <p><strong>Nombre del producto:</strong> ${nombreProducto}</p>
+        <p><strong>Descripción:</strong> ${descripcionProducto}</p>
+        <p><strong>WhatsApp:</strong> <a href="https://api.whatsapp.com/send?phone=+56962810616">Enviar mensaje</a></p>
+      </div>
+    `;
+  
+    // Crear el infowindow del marcador
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+  
+    // Abrir el infowindow cuando se haga clic en el marcador
     marker.addListener('click', () => {
-      this.closeAllInfoWindows();
-      infoWindow.setContent(infoWindowContent); // Aplica la clase CSS aquí
-      infoWindow.open(this.map, marker);
+      infowindow.open(this.map, marker);
     });
-    this.infoWindows.push(infoWindow);
+  
+    // Agregar el infowindow y el marcador a las listas correspondientes
+    this.infoWindows.push(infowindow);
+    this.markers.push(marker);
   }
-  
-  
-  closeAllInfoWindows(){
-    for(let window of this.infoWindows){
-      window.close();
+
+  obtenerUbicacionActual() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const ubicacionActual = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        // Llamar a la función para inicializar el mapa con la ubicación actual
+        this.map.setCenter(ubicacionActual);
+      }, (error) => {
+        console.error('Error al obtener la ubicación actual:', error);
+        // Manejar el error aquí
+      });
+    } else {
+      console.error('La geolocalización no está disponible en este dispositivo.');
+      // Manejar el caso en el que la geolocalización no está disponible
     }
   }
-  buscarEnBaseDeDatos(tipoBusqueda: string, tipoUbicacion: string, terminoBusqueda: string): Observable<any> {
-    // Lógica para buscar en la base de datos aquí...
-    // Por ejemplo, podrías hacer una solicitud HTTP a tu backend.
-    
-    // Supongamos que estás usando HttpClient para realizar la solicitud HTTP
-    return this.http.post<any>('/buscar', { tipoBusqueda, tipoUbicacion, terminoBusqueda });
+
+  // Función para cerrar todos los infowindows
+  closeAllInfoWindows() {
+    this.infoWindows.forEach(infoWindow => {
+      infoWindow.close();
+    });
   }
 
+  // Función para salir de sesión
+  signOut() {
+    this.router.navigate(['/']);
+  }
 }
